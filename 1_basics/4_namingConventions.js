87c48e439d26ebc03acb40aefa8bd6195440f82a
Ss0Mae <ssomae@protonmail.ch>
2024-06-25 21:58:29 +0900

    NamingConventions

---
+++ 1_basics/4_namingConventions.js
@@ -0,0 +1,11 @@
+/**
+ * Naming Conventions
+ * 
+ * 1) 일반적으로 영어를 사용하며 문자와 숫자 모두 사용 할 수 있다.
+ * 2) 숫자로 이름을 시작 할 수 없다
+ * 3) 키워드는 변수명으로 사용 할 수 없다
+ * 4) camelCase
+ * 5) snake_case
+ * 6) PascalCase
+ */
+let codefactory = "codeFactory";
