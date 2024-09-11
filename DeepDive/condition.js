0a633b81c3ff0808abb01b95a10a8ea942e649bb
Ss0Mae <ssomae@protonmail.ch>
2024-09-11 20:02:20 +0900

    Object new Target

--- DeepDive/condition.js
+++ DeepDive/condition.js
@@ -1,1 +1,13 @@
-switch ();
+function Circle(radius) {
+    if (!new.target) {
+        return new Circle(radius);
+    }
+
+    this.radius = radius;
+    this.getDiameter = function () {
+        return 2 * this.radius;
+    }
+}
+
+const circle = new Circle(5);
+console.log(circle.getDiameter());
